7d57ecd632faa506f8083596391d471b58d19b36
Ebuka Ebubedike <ebukaebubedike@rocketmail.com>
2023-09-26 09:59:14 +0000

    Extra line of code for converting city name to lower case removed

--- bikeshare.py
+++ bikeshare.py
@@ -18,8 +18,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input("Input City Name: ")
-        city = city.lower()
+        city = input("Input City Name: ").lower()
         if city not in CITY_DATA.keys():
             print('City not in database, please type another city name')
             continue
